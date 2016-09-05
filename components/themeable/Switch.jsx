a625fbb456e1e5ec3ccf201b8eb6f9e9f50fe6d6
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-09-05 09:59:46 +0200

    Add themeable Switch

---
+++ components/themeable/Switch.jsx
@@ -0,0 +1,11 @@
+import Switch from '../Switch'
+import themeable from '../../lib/decorators/themeable'
+
+export default themeable(Switch, ({ customizations, ...props }) => ({
+  ...props,
+  customize: {
+    ...props.customize,
+    backgroundColor: customizations.color_checkbox,
+    bulletColor: customizations.color_checkbox_checkmark
+  }
+}))
